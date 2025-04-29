c410514a02aa887c45707d8d14d31a7f7d64d374
Dong Hyun <decidere@naver.com>
2025-04-29 23:22:01 +0900

    [Fix] JWT 오류 수정

--- src/module/playlist/playlist.module.ts
+++ src/module/playlist/playlist.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlaylistService } from './playlist.service';
 import { PlaylistController } from './playlist.controller';
+import { JwtStrategy } from '../auth/strategy/jwt.strategy';
 
 @Module({
   controllers: [PlaylistController],
-  providers: [PlaylistService],
+  providers: [PlaylistService, JwtStrategy],
 })
 export class PlaylistModule {}
