462d9066df3a1613160ade69cb9ab2936debf6f3
Dong Hyun <decidere@naver.com>
2025-04-29 21:12:06 +0900

    [Feat] 전체 플레이리스트 조회 로직 구현

--- src/module/playlist/playlist.controller.ts
+++ src/module/playlist/playlist.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PlaylistService } from './playlist.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
 
 @Controller('playlist')
 export class PlaylistController {
   constructor(private readonly playlistService: PlaylistService) {}
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  async getAllPlaylists(@CurrentUserId() userId: number) {
+    return await this.playlistService.getAllPlaylists(userId);
+  }
 }
--- src/module/playlist/playlist.service.ts
+++ src/module/playlist/playlist.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PlaylistService {}
+export class PlaylistService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllPlaylists(userId: number) {
+    const result = await this.prisma.playlist.findMany({
+      orderBy: { id: 'desc' },
+      include: {
+        _count: { select: { PlaylistLike: true } },
+        PlaylistLike: { where: { userId }, select: { id: true } },
+        user: {
+          select: {
+            id: true,
+            email: true,
+            name: true,
+            nickname: true,
+            profile_url: true,
+          },
+        },
+      },
+    });
+
+    const playlists = result.map((res) => this.getPlaylistObj(res));
+
+    return {
+      playlists,
+      message: {
+        code: 200,
+        text: '전체 플레이리스트를 정상적으로 조회했습니다.',
+      },
+    };
+  }
+
+  getPlaylistObj(result: any) {
+    return {
+      userId: result.user.id,
+      userName: result.user.name,
+      userNickname: result.user.nickname,
+      userProfileUrl: result.user.profile_url,
+      postId: result.id,
+      playlistId: result.playlistId,
+      likeCount: result._count.likes,
+      isLiked: !!result.PlaylistLike[0],
+      viewCount: result.viewCount,
+      createdAt: result.createdAt,
+    };
+  }
+}
