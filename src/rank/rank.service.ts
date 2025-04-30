ee0898c30e222b42d37ce16c20cdfc412a3c4ace
Dong Hyun <decidere@naver.com>
2025-05-01 05:16:58 +0900

    [Feat] 스포티파이 API 사용 유저 탑 트랙 조회 로직 구현

--- src/rank/rank.service.ts
+++ src/rank/rank.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class RankService {}
+export class RankService {
+  // 유저 탑 트랙 조회 (스포티파이)
+  async fetchSpotifyTopTracks(userAccessToken: string) {
+    const url = `https://api.spotify.com/v1/me/top/tracks`;
+
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${userAccessToken}`,
+        'Accept-Language': 'ko',
+      },
+    });
+
+    const data = response.data.items.map((item, i) => ({
+      rank: i + 1,
+      trackId: item.id,
+      name: item.name,
+      imageUrl: item.album.images?.[0]?.url || null,
+      artistId: item.artists?.[0]?.id || null,
+      artistName: item.artists?.map((a) => a.name).join(', ') || '',
+      externalUrl: item.external_urls?.spotify || null,
+    }));
+
+    return data;
+  }
+}
