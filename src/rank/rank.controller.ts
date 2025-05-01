c2afcbb79a7a057e6050f663ea4edac4b3ef473b
Dong Hyun <decidere@naver.com>
2025-05-01 20:49:12 +0900

    [Feat] 유저 탑 트랙 조회 로직 구현

--- src/rank/rank.controller.ts
+++ src/rank/rank.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, UseGuards } from '@nestjs/common';
 import { RankService } from './rank.service';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('rank')
 export class RankController {
   constructor(private readonly rankService: RankService) {}
+
+  @Get('track')
+  @UseGuards(AuthGuard('jwt'))
+  async test(
+    @CurrentUserId() userId: number,
+    @Body() body: { code: string; range: string },
+  ) {
+    return await this.rankService.handleUserTopTracks(
+      body.code,
+      userId,
+      body.range,
+    );
+  }
 }
