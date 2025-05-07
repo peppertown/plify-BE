b5793d6acfe9c3eba1319a251765ec6fa50708c4
Dong Hyun <decidere@naver.com>
2025-05-08 06:11:20 +0900

    [Feat] 글로벌 파이프 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 값 제거
+      forbidNonWhitelisted: true, // DTO에 없는 값이 들어오면 에러
+      transform: true, // 자동 타입 변환
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Plify API')
