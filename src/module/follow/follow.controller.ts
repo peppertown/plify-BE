f114bf2a1e4aebe2c098c22fff4f21b291b40a0a
Dong Hyun <decidere@naver.com>
2025-05-09 06:44:55 +0900

    [Feat] 팔로잉 토글 로직 구현

--- src/module/follow/follow.controller.ts
+++ src/module/follow/follow.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { FollowService } from './follow.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
 
 @Controller('follow')
 export class FollowController {
   constructor(private readonly followService: FollowService) {}
+
+  @Post(':targetUserId')
+  @UseGuards(AuthGuard('jwt'))
+  async handleUserFollow(
+    @CurrentUserId() userId: number,
+    @Param('targetUserId', ParseIntPipe) targetUserId: number,
+  ) {
+    return await this.followService.handleUserFollow(userId, targetUserId);
+  }
 }
--- src/module/follow/follow.service.ts
+++ src/module/follow/follow.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class FollowService {}
+export class FollowService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async handleUserFollow(userId: number, targetUserId: number) {
+    try {
+      const existing = await this.prisma.userFollow.findFirst({
+        where: { followeeId: userId, followerId: targetUserId },
+      });
+
+      if (existing) {
+        await this.prisma.userFollow.delete({
+          where: { id: existing.id },
+        });
+      } else {
+        await this.prisma.userFollow.create({
+          data: { followeeId: userId, followerId: targetUserId },
+        });
+      }
+
+      return {
+        message: {
+          code: 200,
+          text: existing
+            ? '팔로잉이 취소되었습니다.'
+            : '팔로우가 완료됐습니다.',
+        },
+      };
+    } catch (err) {
+      console.error('팔로잉 토글 중 에러 발생', err);
+      throw new HttpException(
+        '팔로잉 토글 중 오류가 발생했습니다',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
